249a61d9e87d1f0722b3153a037b9fbde0febd30
Edbert Chandra <160008363+Solynixx@users.noreply.github.com>
2026-01-03 13:23:12 +0700

    Refactor class names and improve semantic structure
    Updated HTML, CSS, and JS to use more descriptive and consistent class names for improved readability and maintainability. Enhanced semantic structure in index.html, updated related selectors in style.css, and adjusted script.js to match new element IDs. No functional changes, but improves code clarity and future scalability.

--- script.js
+++ script.js
@@ -4,31 +4,28 @@ const texts = [
     "Learning by building real projects."
 ];
 
-let index = 0; //Select index from const text 
-let charIndex = 0; // The position of the letter being typed or deleted.
+let index = 0;
+let charIndex = 0;
 let deleting = false;
 let speed = 100;
 
 function type() {
-    const current = texts[index]; //Get the text currently in use based on the index.
+    const current = texts[index];
 
-    if (deleting === false) {
-        // Typing
+    if (!deleting) {
         charIndex++;
-        document.getElementById("typing").textContent = current.slice(0, charIndex);
+        document.getElementById("hero-typing").textContent = current.slice(0, charIndex);
 
         if (charIndex === current.length) {
-            setTimeout( () => deleting = true, 250)    
-        }; // pause before deleting
-    } 
-    else {
-        // Deleting
+            setTimeout(() => deleting = true, 250);
+        }
+    } else {
         charIndex--;
-        document.getElementById("typing").textContent = current.slice(0, charIndex);
+        document.getElementById("hero-typing").textContent = current.slice(0, charIndex);
 
         if (charIndex === 0) {
             deleting = false;
-            index = (index + 1) % texts.length; // move to the next article
+            index = (index + 1) % texts.length;
         }
     }
     setTimeout(type, speed);
